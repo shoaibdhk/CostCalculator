5d4c6787c98b0dbd6458aa54cc4612bac50f12da
Shoaib <shoaib19j@gmail.com>
2019-06-08 20:57:06 -0700

    dhdhd

--- frontend/config.js
+++ frontend/config.js
@@ -1,27 +1,2 @@
-export const perPage = 4
 export const GRAPHQL_URL = `http://localhost:3030`
-export const GRAPHQL_URL_PROD = `https://costcalculator-server.herokuapp.com/`
-export const appName = `Hacker News`
-export const BrandColor = `#e74c3c`
-export const navBar = [
-  {
-    href: '/items',
-    label: 'Items'
-  },
-  {
-    href: '/sell',
-    label: 'Sell'
-  },
-  {
-    href: '/signup',
-    label: 'Signup'
-  },
-  {
-    href: '/orders',
-    label: 'Orders'
-  },
-  {
-    href: '/me',
-    label: 'Account'
-  }
-]
+export const GRAPHQL_URL_PROD = 'https://costcalculator-server.herokuapp.com'
