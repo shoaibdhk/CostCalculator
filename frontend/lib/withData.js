18a1d4a3cb58a2c4aa5e92b179a62c7de7c2c5c6
Shoaib <shoaib19j@gmail.com>
2019-06-10 19:33:36 -0700

    some change in withData.js

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,13 +1,15 @@
 import withApollo from 'next-with-apollo'
-import ApolloClient from 'apollo-boost'
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
 import { GRAPHQL_URL, GRAPHQL_URL_PROD } from '../config'
 
-function createClient({ headers }) {
+function createClient({ headers, initialState }) {
   return new ApolloClient({
-    credentials: 'include',
+    // credentials: 'include',
     uri: process.env.NODE_ENV === 'development' ? GRAPHQL_URL : GRAPHQL_URL_PROD,
+    cache: new InMemoryCache().restore(initialState || {}),
     request: operation => {
       operation.setContext({
+        credentials: 'include',
         headers
       })
     }
