bb7064368303ee17c3df3b4b59e900744ffae864
Shoaib <shoaib19j@gmail.com>
2019-06-13 21:53:33 -0700

    some changes

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,5 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import Head from 'next/head'
 import PleaseSignIn from '../components/auth/PleaseSignIn'
-import Router from 'next/router'
-import redirect from '../lib/redirect'
-import { CURRENT_USER_QUERY } from '../components/auth/User'
 import Dashboard from '../components/layout/Dashboard'
-const Header = styled.h1`
-  color: red;
-`
 // class Home extends Component {
 //     static async getInitialProps({ res, apolloClient }) {
 //       const { data } = await apolloClient.query({ query: CURRENT_USER_QUERY })
