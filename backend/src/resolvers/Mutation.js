f57d93ee97f02c420ab2c611817f15547875a3a8
Shoaib Sharif <sharifshoaib@yahoo.com>
2019-08-17 13:14:02 -0700

    cookie response

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,7 +1,7 @@
-const { hash, compare } = require('bcryptjs')
-const Joi = require('@hapi/joi')
-const { sign } = require('jsonwebtoken')
-const { randomBytes } = require('crypto')
+const { hash, compare } = require('bcryptjs');
+const Joi = require('@hapi/joi');
+const { sign } = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
 
 const signUpSchema = Joi.object().keys({
   name: Joi.string()
@@ -23,7 +23,7 @@ const signUpSchema = Joi.object().keys({
     .max(14)
     .required()
     .label('Password')
-})
+});
 const signInSchema = Joi.object().keys({
   email: Joi.string()
     .email()
@@ -40,7 +40,7 @@ const signInSchema = Joi.object().keys({
     .min(6)
     .max(14)
     .required()
-})
+});
 
 const createPostSchema = Joi.object().keys({
   title: Joi.string().required(),
@@ -53,7 +53,7 @@ const createPostSchema = Joi.object().keys({
         .required()
     })
   )
-})
+});
 const updatePostSchema = Joi.object().keys({
   title: Joi.string(),
   description: Joi.string(),
@@ -64,97 +64,99 @@ const updatePostSchema = Joi.object().keys({
     })
   ),
   postId: Joi.string()
-})
+});
 const Mutation = {
   async signup(parent, args, { prisma, response }, info) {
-    const { error } = Joi.validate({ ...args }, signUpSchema, { abortEarly: false })
+    const { error } = Joi.validate({ ...args }, signUpSchema, { abortEarly: false });
 
     if (!error) {
-      const password = await hash(args.password, 10)
-      const user = await prisma.createUser({ ...args, password })
-      let token = sign({ userId: user.id }, process.env.APP_SECRET)
+      const password = await hash(args.password, 10);
+      const user = await prisma.createUser({ ...args, password });
+      let token = sign({ userId: user.id }, process.env.APP_SECRET);
       await response.cookie('token', token, {
         httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 365
-      })
-      return user
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        secure: true
+      });
+      return user;
     }
 
-    const errors = {}
-    error.details.map(err => (errors[err.path[0]] = err.message.replace(/"/g, '')))
-    throw new Error(JSON.stringify(errors))
+    const errors = {};
+    error.details.map(err => (errors[err.path[0]] = err.message.replace(/"/g, '')));
+    throw new Error(JSON.stringify(errors));
   },
   async signin(parent, args, { prisma, response }, info) {
-    const { error } = Joi.validate({ ...args }, signInSchema, { abortEarly: false })
+    const { error } = Joi.validate({ ...args }, signInSchema, { abortEarly: false });
 
     if (!error) {
-      const user = await prisma.user({ email: args.email })
+      const user = await prisma.user({ email: args.email });
 
-      let isMatched, token
+      let isMatched, token;
       if (user) {
-        isMatched = await compare(args.password, user.password)
-      } else throw new Error(JSON.stringify({ email: 'No user found' }))
+        isMatched = await compare(args.password, user.password);
+      } else throw new Error(JSON.stringify({ email: 'No user found' }));
       if (isMatched) {
-        token = sign({ userId: user.id }, process.env.APP_SECRET)
+        token = sign({ userId: user.id }, process.env.APP_SECRET);
         await response.cookie('token', token, {
           httpOnly: true,
-          maxAge: 1000 * 60 * 60 * 24 * 365
-        })
-        return user
-      } else throw new Error(JSON.stringify({ password: `Password didn't match` }))
+          maxAge: 1000 * 60 * 60 * 24 * 365,
+          secure: true
+        });
+        return user;
+      } else throw new Error(JSON.stringify({ password: `Password didn't match` }));
     }
 
-    const errors = {}
-    error.details.map(err => (errors[err.path[0]] = err.message.replace(/"/g, '')))
-    throw new Error(JSON.stringify(errors))
+    const errors = {};
+    error.details.map(err => (errors[err.path[0]] = err.message.replace(/"/g, '')));
+    throw new Error(JSON.stringify(errors));
   },
   signout(parent, args, { response }, info) {
-    response.clearCookie('token')
-    return true
+    response.clearCookie('token');
+    return true;
   },
 
   async createPost(parent, args, { request, prisma }, info) {
-    if (!request.userId) throw new Error('No user found')
-    const { error } = Joi.validate({ ...args }, createPostSchema, { abortEarly: false })
+    if (!request.userId) throw new Error('No user found');
+    const { error } = Joi.validate({ ...args }, createPostSchema, { abortEarly: false });
     if (error) {
-      const errors = {}
-      error.details.map(err => (errors[err.path[0]] = err.message.replace(/"/g, '')))
-      throw new Error(JSON.stringify(errors))
+      const errors = {};
+      error.details.map(err => (errors[err.path[0]] = err.message.replace(/"/g, '')));
+      throw new Error(JSON.stringify(errors));
     }
     const post = await prisma.createPost({
       ...args,
       costs: { create: [...args.costs] },
       author: { connect: { id: request.userId } }
-    })
-    return post
+    });
+    return post;
   },
 
   async deletePost(parent, args, { request, prisma }, info) {
     if (!request.userId) {
-      throw new Error('No user found')
+      throw new Error('No user found');
     }
-    const user = await prisma.post({ id: args.postId }).author()
+    const user = await prisma.post({ id: args.postId }).author();
     if (user.id === request.userId) {
-      const post = await prisma.deletePost({ id: args.postId })
-      return post
+      const post = await prisma.deletePost({ id: args.postId });
+      return post;
     } else {
-      throw new Error('No posts found')
+      throw new Error('No posts found');
     }
   },
   async updatePost(parent, args, { prisma, request }, info) {
     if (!request.userId) {
-      throw new Error('No user found')
+      throw new Error('No user found');
     }
-    const { error } = Joi.validate({ ...args }, updatePostSchema, { abortEarly: false })
+    const { error } = Joi.validate({ ...args }, updatePostSchema, { abortEarly: false });
     if (error) {
-      const errors = {}
-      error.details.map(err => (errors[err.path[0]] = err.message.replace(/"/g, '')))
-      throw new Error(JSON.stringify(errors))
+      const errors = {};
+      error.details.map(err => (errors[err.path[0]] = err.message.replace(/"/g, '')));
+      throw new Error(JSON.stringify(errors));
     }
-    const user = await prisma.post({ id: args.postId }).author()
+    const user = await prisma.post({ id: args.postId }).author();
     if (user.id === request.userId) {
       if (args.costs) {
-        const costs = await prisma.deleteManyCostCalculates({ id_gt: 'a' })
+        const costs = await prisma.deleteManyCostCalculates({ id_gt: 'a' });
         const post = await prisma.updatePost({
           where: { id: args.postId },
           data: {
@@ -164,8 +166,8 @@ const Mutation = {
               create: [...args.costs]
             }
           }
-        })
-        return post
+        });
+        return post;
       }
       const post = await prisma.updatePost({
         where: { id: args.postId },
@@ -173,12 +175,12 @@ const Mutation = {
           title: args.title,
           description: args.description
         }
-      })
-      return post
+      });
+      return post;
     } else {
-      throw new Error('No posts found')
+      throw new Error('No posts found');
     }
   }
-}
+};
 
-module.exports = Mutation
+module.exports = Mutation;
